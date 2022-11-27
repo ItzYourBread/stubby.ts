07fe162a91a40b97af4321a73200bf0c1769fb71
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-11-27 19:44:46 +0600

    Update getRandomArray.ts

--- src/lib/getRandomArray.ts
+++ src/lib/getRandomArray.ts
@@ -1,5 +1,5 @@
 export function getRandomArray(arr: string) {
-  const randomIndex = Math.floor(Math.random() * arr.length);
-  const item = arr[randomIndex];
-  return item;
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[randomIndex];
+    return item;
 }
