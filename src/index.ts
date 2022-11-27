708cb3756177055de58e866311da22b7dbdc4bc8
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-11-27 19:47:20 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 /**
- * @param - first value is minimum ans second value is maximum.
- **/
-import { getRandomNumber as RandomNumber } from './lib/getRandomNumber';
-export { RandomNumber };
+* @param - first value is minimum ans second value is maximum.
+**/
+import {
+    getRandomNumber as RandomNumber
+} from './lib/getRandomNumber';
+
 
 /**
- * @object - the array object.
- **/
-import { getRandomArray as RandomArray } from './lib/getRandomArray';
-export { RandomArray };
+* @object - the array object.
+**/
+import {
+    getRandomArray as RandomArray
+} from './lib/getRandomArray';
+
+
+export {
+    RandomNumber,
+    RandomArray
+
