85118ca64de9424b23bb9ebd1d2edc6b0d093b63
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-12-29 03:01:40 +0600

    Update RandomArray.js

--- examples/RandomArray.js
+++ examples/RandomArray.js
@@ -6,3 +6,12 @@ const fruits = ['Apple', 'Pineapple', 'Orange', 'Strawberry', 'Mango', 'Cherry']
 // Use the RandomArray function to get a random fruit from the array
 const randomFruit = stubby.RandomArray(fruits);
 console.log(randomFruit); // Outputs a random fruit from the array
+
+const names = [
+  { name: 'Arif' },
+  { name: 'Afrin' }
+];
+
+// Use the RandomArray function to get a random object from the array
+const randomName = stubby.RandomArray(names);
+console.log(randomName.name); // Outputs a random name from the array
