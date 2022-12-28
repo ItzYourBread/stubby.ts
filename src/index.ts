214e57c6912c53c3407cfa632570a757a30d7f3a
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-12-28 23:19:59 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
-/**
- * @param - first value is minimum ans second value is maximum.
- **/
 import { getRandomNumber as RandomNumber } from './lib/getRandomNumber';
-
-/**
- * @object - the array object.
- **/
 import { getRandomArray as RandomArray } from './lib/getRandomArray';
-
-/**
- * @param - first value is count and second value is digits
- **/
 import { toSmallNumber as SmallNumber } from './lib/toSmallNumber';
 
 export default {
@@ -18,4 +7,5 @@ export default {
   RandomArray,
   SmallNumber,
 };
+
 export { RandomNumber, RandomArray, SmallNumber };
