6ccff7e9aa272ae225d774eed023b82e85961933
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-12-28 23:13:40 +0600

    Update getRandomArray.ts

--- src/lib/getRandomArray.ts
+++ src/lib/getRandomArray.ts
@@ -1,4 +1,19 @@
-export function getRandomArray(arr: any) {
+/**
+ * Returns a random element from an array.
+ *
+ * @param {Array} arr - The array to select a random element from.
+ * @returns {*} A random element from the input array.
+ *
+ * @throws {TypeError} If the input parameter is not an array.
+ * @throws {Error} If the input array is empty.
+ */
+export function getRandomArray(arr: any[]): any {
+  if (!Array.isArray(arr)) {
+    throw new TypeError('The input parameter must be an array');
+  }
+  if (arr.length === 0) {
+    throw new Error('The input array is empty');
+  }
   const randomIndex = Math.floor(Math.random() * arr.length);
   const item = arr[randomIndex];
   return item;
