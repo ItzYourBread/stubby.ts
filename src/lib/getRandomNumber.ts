6ed09ea0e8c93d275a1bb2e669950c2d9702d6e5
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-12-28 23:15:45 +0600

    Update getRandomNumber.ts

--- src/lib/getRandomNumber.ts
+++ src/lib/getRandomNumber.ts
@@ -1,5 +1,19 @@
-export function getRandomNumber(min: any, max: any) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+/**
+ * Returns a random number between two numbers (inclusive).
+ *
+ * @param {number} min - The minimum number.
+ * @param {number} max - The maximum number.
+ * @returns {number} A random number between the minimum and maximum numbers (inclusive).
+ *
+ * @throws {TypeError} If any of the input parameters is not a number.
+ * @throws {RangeError} If the minimum number is greater than the maximum number.
+ */
+export function getRandomNumber(min: number, max: number): number {
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    throw new TypeError('The input parameters must be numbers');
+  }
+  if (min > max) {
+    throw new RangeError('The minimum number must be less than or equal to the maximum number');
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
