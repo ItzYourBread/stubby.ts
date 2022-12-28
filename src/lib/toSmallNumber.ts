25a467a14504c9550509c036103fab844d1c8a1a
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-12-28 23:11:26 +0600

    Update toSmallNumber.ts

--- src/lib/toSmallNumber.ts
+++ src/lib/toSmallNumber.ts
@@ -1,6 +1,19 @@
 let numbers = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'];
 
-export function toSmallNumber(count: number, digits: number) {
+/**
+ * Converts a number to a small number representation using superscript digits.
+ *
+ * @param {number} count - The number to convert.
+ * @param {number} digits - The number of digits to use in the small number representation.
+ * @returns {string} The small number representation of the input number.
+ *
+ * @throws {TypeError} If any of the input parameters is not a positive integer.
+ */
+export function toSmallNumber(count: number, digits: number): string {
+  if (!Number.isInteger(count) || !Number.isInteger(digits) || count < 0 || digits < 0) {
+    throw new TypeError('The input parameters must be positive integers');
+  }
+
   var result = '';
   var num = count;
   var i = 0;
