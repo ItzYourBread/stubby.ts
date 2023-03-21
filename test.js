c5e48a5d84aedf9f151af9e55db3c40a865ae3d9
Bread <72562619+ItzYourBread@users.noreply.github.com>
2023-03-21 15:58:11 +0000

    Test

--- test.js
+++ test.js
@@ -1,31 +1,3 @@
-const { RandomNumber, RandomArray, SmallNumber, SystemInfo, Replace } = require('./dist/index');
+const { config } = require("./dist/index")
 
-// generate a random number between 10 and 100
-const randomNumber = RandomNumber(10, 100);
-console.log(randomNumber);
-
-// generate a random fruit from the array
-const fruits = ['Apple', 'Pineapple', 'Orange', 'Strawberry', 'Mango', 'Cherry'];
-const randomFruit = RandomArray(fruits);
-console.log(randomFruit);
-
-// convert a number to superscript
-const num = 143;
-const digits = num.toString().length + 1;
-console.log(SmallNumber(num, digits));
-
-// get system statistics
-console.log(SystemInfo().memory());
-console.log(SystemInfo().memoryUsage());
-console.log(SystemInfo().cpuUsage());
-console.log(SystemInfo().cores());
-console.log(SystemInfo().cpuBrand());
-console.log(SystemInfo().uptime());
-
-let paragraph = "Hello my name ia %name%, im %age% years old, and most peoples like me to call %nickname%.";
-let replacements = {
-    '%name%': 'Arif',
-    '%age%': '16',
-    '%nickname%': 'Bread'
-};
-console.log(Replace(paragraph, replacements));
+console.log(config);
